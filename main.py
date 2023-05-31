bc1b1f61710a2aca7c2b1d714e5c244733eeee6c
dongjin05 <ehdwlsshin@gmail.com>
2023-05-31 15:53:35 +0900

    test

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_dongguk():
-   return 'Hello Dongguk University Students!'
+def test():
+   return 'git test'
 
 if __name__ == '__main__':
    app.run()
