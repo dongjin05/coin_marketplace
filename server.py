68bf368b9545202c68a1cfd13f54dfc64ffc42a4
dongjin05 <ehdwlsshin@gmail.com>
2023-06-05 15:14:10 +0900

    login, balance 수정

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 from pymongo import MongoClient
+import secrets
 
 app = Flask(__name__)
 client = MongoClient('mongodb+srv://ehdwlsshin:1234@cluster0.c5tc90g.mongodb.net/')  # MongoDB 연결 설정
 db = client['coin_market']  # 데이터베이스 선택
 users_collection = db['users']  # 사용자 정보 컬렉션 선택
 
+secret_key = secrets.token_hex(16)
+app.secret_key = secret_key
 
 
 # 메인 페이지
@@ -14,8 +17,19 @@ def main():
     return render_template('main.html')
 
 # 로그인 페이지
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user_data = users_collection.find_one({'username': username})
+        if user_data and user_data['password'] == password:
+            session['username'] = username
+            return redirect(url_for('main'))
+        else:
+            return "Invalid username or password."
+
     return render_template('login.html')
 
 # 회원 가입 페이지
@@ -33,7 +47,52 @@ def signup():
 # 잔고 페이지
 @app.route('/balance')
 def balance():
-    return render_template('balance.html')
+    if 'username' in session:
+        username = session['username']
+        user_data = users_collection.find_one({'username': username})
+        if user_data:
+            balance = user_data['balance']
+            return render_template('balance.html', balance=balance)
+        else:
+            return "User not found."
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/add_money', methods=['POST'])
+def add_money():
+    username = request.form['username']
+    amount = float(request.form['amount'])
+
+    user_data = users_collection.find_one({'username': username})
+    if user_data:
+        current_balance = user_data['balance']
+        updated_balance = current_balance + amount
+
+        users_collection.update_one({'username': username}, {'$set': {'balance': updated_balance}})
+
+        return "Money added successfully."
+    else:
+        return "User not found."
+
+@app.route('/withdraw_money', methods=['POST'])
+def withdraw_money():
+    username = request.form['username']
+    amount = float(request.form['amount'])
+
+    user_data = users_collection.find_one({'username': username})
+    if user_data:
+        current_balance = user_data['balance']
+        if amount <= current_balance:
+            updated_balance = current_balance - amount
+
+            users_collection.update_one({'username': username}, {'$set': {'balance': updated_balance}})
+
+            return "Money withdrawn successfully."
+        else:
+            return "Insufficient balance."
+    else:
+        return "User not found."
+
 
 # 마켓 페이지
 @app.route('/market')
